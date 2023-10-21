032560334fb07369f84e987d733e97c58d7954a0
Oiso <test@test.com>
2023-10-21 15:14:51 +0900

    bedrock

--- samples/incoming-webhook/nodejs/api/server/index.js
+++ samples/incoming-webhook/nodejs/api/server/index.js
@@ -1,33 +1,41 @@
+const { Bedrock } = require('langchain/llms/bedrock');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const server = express();
-const axios = require('axios');
 server.use(express.json());
 server.use(express.urlencoded({
     extended: true
 }));
-
+const model = new Bedrock({
+    model: 'anthropic.claude-v2',
+    region: 'us-east-1'
+});
 
 server.get('/', async (req, res) => {
-    console.log(req)
-  res.send(`Hi! Node.js version: ${process.version}`)
-})
+    console.log(req);
+    res.send(`Hi! Node.js version: ${ process.version }`);
+});
 
 server.post('/', async (req, res) => {
-        console.log(req.headers)
-        console.log(req.body)
-    
+    console.log(req.headers);
+    console.log(req.body);
     const body = req.body;
+
+    const text = await model.invoke(prompting(body));
+
     const response = {
-        'type': 'message',
-        'text': 'hello! Mr. ' + body.from.name + '. You said : ' + body.text
+        type: 'message',
+        text: text
     };
-    console.log(response)
+    console.log(response);
 
+    res.json(response);
+});
 
-  res.json(response)
-})
+function prompting(body) {
+    return `\n\nHuman: 以下の質問に答えて<question>${ body }</question>\n\nAssistant:`;
+}
 
 server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${ PORT }`);
 });
